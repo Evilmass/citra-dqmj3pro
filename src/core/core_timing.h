8656201fa72335205655e77efccb79f6d57eae76
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-07-04 21:36:38 +0800

    2500

--- src/core/core_timing.h
+++ src/core/core_timing.h
@@ -177,7 +177,7 @@ public:
     // But we never want to schedule events into the current slice, because then cores might to
     // run small slices to sync up again. This is especially important for events that are always
     // scheduled and repated.
-    static constexpr int MAX_SLICE_LENGTH = 16000;
+    static constexpr int MAX_SLICE_LENGTH = 2500;
 
     class Timer {
     public:
