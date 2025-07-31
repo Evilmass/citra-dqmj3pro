11630252d76ceefc8e8047f11450df360f6f0f1e
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-08-01 00:29:32 +0800

    7z pack once

--- .ci/pack.sh
+++ .ci/pack.sh
@@ -7,42 +7,12 @@ REV_NAME="citra-${OS}-${TARGET}-${GITDATE}-${GITREV}"
 # Find out what release we are building
 if [[ "$GITHUB_REF_NAME" =~ ^canary- ]] || [[ "$GITHUB_REF_NAME" =~ ^nightly- ]]; then
     RELEASE_NAME=$(echo $GITHUB_REF_NAME | cut -d- -f1)
-    # For compatibility with existing installs, use mingw/osx in the archive and target names.
-    if [ "$TARGET" = "msys2" ]; then
-        REV_NAME="citra-${OS}-mingw-${GITDATE}-${GITREV}"
-        RELEASE_NAME="${RELEASE_NAME}-mingw"
-    elif [ "$OS" = "macos" ]; then
-        REV_NAME="citra-osx-${TARGET}-${GITDATE}-${GITREV}"
-    fi
 else
     RELEASE_NAME=head
 fi
 
 mkdir -p artifacts
-
-if [ -z "${UPLOAD_RAW}" ]; then
-    # Archive and upload the artifacts.
-    mkdir "$REV_NAME"
-    mv build/bundle/* "$REV_NAME"
-
-    if [ "$OS" = "windows" ]; then
-        ARCHIVE_NAME="${REV_NAME}.zip"
-        # powershell Compress-Archive "$REV_NAME" "$ARCHIVE_NAME"
-    else
-        ARCHIVE_NAME="${REV_NAME}.tar.gz"
-        tar czvf "$ARCHIVE_NAME" "$REV_NAME"
-    fi
-
-    mv "$REV_NAME" $RELEASE_NAME
-    7z a "$REV_NAME.7z" $RELEASE_NAME
-
-    # mv "$ARCHIVE_NAME" artifacts/
-    mv "$REV_NAME.7z" artifacts/
-else
-    # Directly upload the raw artifacts, renamed with the revision.
-    for ARTIFACT in build/*; do
-        FILENAME=$(basename "$ARTIFACT")
-        EXTENSION="${FILENAME##*.}"
-        mv "$ARTIFACT" "artifacts/$REV_NAME.$EXTENSION"
-    done
-fi
+mkdir "$REV_NAME"
+mv build/bundle/* "$RELEASE_NAME"
+7z a "$REV_NAME.7z" $RELEASE_NAME
+mv "$REV_NAME.7z" artifacts/
